cbff25cd66261cecd637059b3de912a1ff7846df
halstedlarsson <halsted.larsson@gmail.com>
2016-05-10 11:05:50 -0400

    Search cell

---
+++ src/oxford/CellSearch.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class CellSearch
+{
+
+	public HashSet<Cell> CellGroup(Cell startingPoint) {
+		HashSet<Cell> marked = new HashSet<Cell> ();
+		CheckCellGroupFunc (startingPoint, marked);
+		return marked;
+	}
+
+	private void CheckCellGroupFunc(Cell startingPoint, HashSet<Cell> marked) {
+
+		if (marked.Contains (startingPoint)) {
+			return;
+		}
+		marked.Add (this);
+		foreach (Cell neighbor in startingPoint.neighbors) {
+			CellGroup (neighbor, marked);
+		}
+		return;
+	}
+}
+
